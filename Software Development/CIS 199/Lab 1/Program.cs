441fca55205b49f7ae5e0a239a923e4a49eaa436
Emily Wantland <50221752+emilywantland@users.noreply.github.com>
2020-11-06 12:35:42 -0500

    Add files via upload

---
+++ Software Development/CIS 199/Lab 1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Grading ID: N2466
+//Section: CIS 199-75
+//Lab: 1
+//Due Date: January 20, 2019
+//This file lists my grading ID, hobbies, and favorite book and movie.
+
+using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Grading ID:      N2466");
+            Console.WriteLine("Hobbies:         Entertaining friends and reading");
+            Console.WriteLine("Favorite Book:   A Thousand Splendid Suns");
+            Console.WriteLine("Favorite Movie:  Wonder Woman");
+        }
+    }
+}
