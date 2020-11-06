6561cf8e77b631ddbec26f2dacbf494848146bc8
Emily Wantland <50221752+emilywantland@users.noreply.github.com>
2020-11-06 12:48:29 -0500

    Add files via upload

---
+++ Software Development/CIS 199/Program 4/GroundPackage.cs
@@ -0,0 +1,197 @@
+﻿// Grading ID: N2466
+// Section: CIS 199-75
+// Due Date: 4-23-2019
+// Program: 4
+// Description: Displays original package information and changes data
+
+using System;
+
+namespace Program4
+{
+    public class GroundPackage // Doesn't produce output, does calculations
+    {
+        // Declaring variables
+        public int _originZip;      // Package's origin zip
+        public int _destinationZip; // Package's destination zip
+        public double _length;      // Package's length
+        public double _width;       // Package's width
+        public double _height;      // Package's height
+        public double _weight;      // Package's weight
+        public int _zoneDistance;   // Zone distance
+
+        const double SIZE_COST_FACTOR = 0.25;   // Size cost factor constant variable
+        const double WEIGHT_COST_FACTOR = 0.45; // Weight cost factor constant variable
+
+        public int OriginZip
+        {
+            // Precondition:  None
+            // Postcondition: The packages's origin zip is returned
+            get { return _originZip; }
+
+            // Precondition:  The value must be greater or equal to 00000 and less than or equal to 99999
+            // Postcondition: The packages's origin zip is set to the specified value
+            set
+            {
+                if (value >= 00000 && value <= 99999)
+                {
+                    _originZip = value;
+                }
+                else
+                {
+                    _originZip = 40202;
+                }
+            }
+        }
+
+        public int DestinationZip
+        {
+            // Precondition:  None
+            // Postcondition: The packages's destination zip is returned
+            get { return _destinationZip; }
+
+            // Precondition:  The value must be greater or equal to 00000 and less than or equal to 99999
+            // Postcondition: The packages's destination zip is set to the specified value
+            set
+            {
+                if (value >= 00000 && value <= 99999)
+                {
+                    _destinationZip = value;
+                }
+                else
+                {
+                    _destinationZip = 90210;
+                }
+            }
+        }
+
+        public double Length
+        {
+            // Precondition:  None
+            // Postcondition: The packages's Length is returned
+            get { return _length; }
+
+            // Precondition:  The value must be greater than 0
+            // Postcondition: The packages's length is set to the specified value
+            set
+            {
+                if (value > 0)
+                {
+                    _length = value;
+                }
+                else
+                {
+                    _length = 1.0;
+                }
+            }
+        }
+
+        public double Width
+        {
+            // Precondition:  None
+            // Postcondition: The packages's weight is returned
+            get { return _width; }
+
+            // Precondition:  The value must be greater than 0
+            // Postcondition: The packages's weight is set to the specified value
+            set
+            {
+                if (value > 0)
+                {
+                    _width = value;
+                }
+                else
+                {
+                    _width = 1.0;
+                }
+            }
+        }
+
+        public double Height
+        {
+            // Precondition:  None
+            // Postcondition: The packages's width is returned
+            get { return _height; }
+
+            // Precondition:  The value must be greater than 0
+            // Postcondition: The packages's width is set to the specified value
+            set
+            {
+                if (value > 0)
+                {
+                    _height = value;
+                }
+                else
+                {
+                    _height = 1.0;
+                }
+            }
+        }
+
+        public double Weight
+        {
+            // Precondition:  None
+            // Postcondition: The packages's weight is returned
+            get { return _weight; }
+
+            // Precondition:  The value must be greater than 0
+            // Postcondition: The packages's weight is set to the specified value
+            set
+            {
+                if (value > 0)
+                {
+                    _weight = value;
+                }
+                else
+                {
+                    _weight = 1.0;
+                }
+            }
+        }
+
+        // Precondition:  None
+        // Postcondition: The package is constructed
+        public GroundPackage(int originZip, int destinationZip, double length, 
+                             double width, double height, double weight)
+        {
+            OriginZip = originZip;
+            DestinationZip = destinationZip;
+            Length = length;
+            Width = width;
+            Height = height;
+            Weight = weight;
+        }
+
+        // Precondition: None
+        // Postcondition: The zone distance is calculated
+        public int ZoneDist
+        {
+            get
+            {
+                return Math.Abs((OriginZip / 10000) - (DestinationZip / 10000));
+            }
+        }
+
+        // Virtual method to be overridden by derived classes
+        // Precondition:  None
+        // Postcondition: The package's cost is calculated and returned
+        public virtual double CalcCost()
+        {
+            double cost = SIZE_COST_FACTOR * (Length + Width + Height)
+                          + WEIGHT_COST_FACTOR * (ZoneDist + 1) * (Weight);
+            return cost;
+        }
+
+        // Precondition:  None
+        // Postcondition: The packages's information is returned as a formatted string
+        public override string ToString() // Override is required!
+        {
+            return $"Origin Zip:      {OriginZip}{Environment.NewLine}" +
+                   $"Destination Zip: {DestinationZip}{Environment.NewLine}" +
+                   $"Package Length:  {Length}{Environment.NewLine}" +
+                   $"Package Width:   {Width}{Environment.NewLine}" +
+                   $"Package Height:  {Height}{Environment.NewLine}" +
+                   $"Package Weight:  {Weight}{Environment.NewLine}" +
+                   $"Zone Distance:   {ZoneDist}";
+        }
+    }
+}
---
+++ Software Development/CIS 199/Program 4/TestApplication.cs
@@ -0,0 +1,66 @@
+﻿// Grading ID: N2466
+// Section: CIS 199-75
+// Due Date: 4-23-2019
+// Program: 4
+// Description: Displays original package information and changes data
+
+using static System.Console;
+
+namespace Program4
+{
+    public class TestApplication
+    {
+        public static void Main(string[] args)
+        {
+            // Create derived class objects
+            GroundPackage package1 = new GroundPackage(40220, 95993, 4.5, 3.0, 8.3, 20.7);
+            GroundPackage package2 = new GroundPackage(33594, 19061, 29.1, 18.4, 9.6, 47.3);
+            GroundPackage package3 = new GroundPackage(58078, 07740, 3.8, 4.7, 9.3, 10.1);
+            GroundPackage package4 = new GroundPackage(38637, 97603, 2.6, 1.9, 3.5, 2.1);
+            GroundPackage package5 = new GroundPackage(22030, 77478, 6.7, 11.4, 7.3, 3.6);
+
+            // Create five-element GroundPackage array
+            GroundPackage[] packages = new GroundPackage[5];
+
+            // Initialize array with Packages of derived types
+            packages[0] = package1;
+            packages[1] = package2;
+            packages[2] = package3;
+            packages[3] = package4;
+            packages[4] = package5;
+
+            // Calling method
+            WriteLine("---------------------");
+            WriteLine("-   Original Data   -");
+            WriteLine("---------------------");
+            WriteLine();
+            DisplayPackages(packages);
+
+            // Changing data
+            package1.Length = 0.1;
+            package2.Height = 21.2;
+            package3.OriginZip = 40208;
+            package4.DestinationZip = 12345;
+            package5.Weight = 10.2;
+
+            // Calling method
+            WriteLine("---------------------");
+            WriteLine("-   After Changes   -");
+            WriteLine("---------------------");
+            WriteLine();
+            DisplayPackages(packages);
+        }
+
+        // Precondition: None
+        // Postcondition: The packages's information and cost is returned as a formatted string
+        public static void DisplayPackages(GroundPackage[] packages)
+        {
+            foreach (GroundPackage currentPackage in packages)
+            { 
+                WriteLine($"{currentPackage}"); // invokes ToString implicitly
+                WriteLine($"Cost:            {currentPackage.CalcCost():C}"); // Wrestled with spacing
+                WriteLine();                                                  // and gave up :P
+            }
+        }
+    }
+}
