05a754d7d7e1b2c4bd39ac76af2cbae95ac0b3dd
Emily Wantland <50221752+emilywantland@users.noreply.github.com>
2020-11-06 12:47:26 -0500

    Add files via upload

---
+++ Software Development/CIS 199/Program 3/Program 3 Form.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace Program3
+{
+    partial class Prog3Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.taxOutLbl = new System.Windows.Forms.Label();
+            this.taxLbl = new System.Windows.Forms.Label();
+            this.marginalRateOutLbl = new System.Windows.Forms.Label();
+            this.marginalRateLbl = new System.Windows.Forms.Label();
+            this.calcTaxBtn = new System.Windows.Forms.Button();
+            this.filingBox = new System.Windows.Forms.GroupBox();
+            this.headOfHouseRdoBtn = new System.Windows.Forms.RadioButton();
+            this.jointlyRdoBtn = new System.Windows.Forms.RadioButton();
+            this.separatelyRdoBtn = new System.Windows.Forms.RadioButton();
+            this.singleRdoBtn = new System.Windows.Forms.RadioButton();
+            this.incomeTxt = new System.Windows.Forms.TextBox();
+            this.incomeLbl = new System.Windows.Forms.Label();
+            this.titleLbl = new System.Windows.Forms.Label();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.filingBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // taxOutLbl
+            // 
+            this.taxOutLbl.BackColor = System.Drawing.SystemColors.Window;
+            this.taxOutLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.taxOutLbl.Location = new System.Drawing.Point(203, 436);
+            this.taxOutLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.taxOutLbl.Name = "taxOutLbl";
+            this.taxOutLbl.Size = new System.Drawing.Size(149, 30);
+            this.taxOutLbl.TabIndex = 17;
+            // 
+            // taxLbl
+            // 
+            this.taxLbl.AutoSize = true;
+            this.taxLbl.Location = new System.Drawing.Point(95, 438);
+            this.taxLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.taxLbl.Name = "taxLbl";
+            this.taxLbl.Size = new System.Drawing.Size(95, 20);
+            this.taxLbl.TabIndex = 16;
+            this.taxLbl.Text = "Income Tax:";
+            // 
+            // marginalRateOutLbl
+            // 
+            this.marginalRateOutLbl.BackColor = System.Drawing.SystemColors.Window;
+            this.marginalRateOutLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.marginalRateOutLbl.Location = new System.Drawing.Point(203, 388);
+            this.marginalRateOutLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.marginalRateOutLbl.Name = "marginalRateOutLbl";
+            this.marginalRateOutLbl.Size = new System.Drawing.Size(149, 30);
+            this.marginalRateOutLbl.TabIndex = 15;
+            // 
+            // marginalRateLbl
+            // 
+            this.marginalRateLbl.AutoSize = true;
+            this.marginalRateLbl.Location = new System.Drawing.Point(49, 390);
+            this.marginalRateLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.marginalRateLbl.Name = "marginalRateLbl";
+            this.marginalRateLbl.Size = new System.Drawing.Size(141, 20);
+            this.marginalRateLbl.TabIndex = 14;
+            this.marginalRateLbl.Text = "Marginal Tax Rate:";
+            // 
+            // calcTaxBtn
+            // 
+            this.calcTaxBtn.Location = new System.Drawing.Point(68, 334);
+            this.calcTaxBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.calcTaxBtn.Name = "calcTaxBtn";
+            this.calcTaxBtn.Size = new System.Drawing.Size(124, 35);
+            this.calcTaxBtn.TabIndex = 13;
+            this.calcTaxBtn.Text = "Calculate Tax";
+            this.calcTaxBtn.UseVisualStyleBackColor = true;
+            this.calcTaxBtn.Click += new System.EventHandler(this.calcTaxBtn_Click);
+            // 
+            // filingBox
+            // 
+            this.filingBox.Controls.Add(this.headOfHouseRdoBtn);
+            this.filingBox.Controls.Add(this.jointlyRdoBtn);
+            this.filingBox.Controls.Add(this.separatelyRdoBtn);
+            this.filingBox.Controls.Add(this.singleRdoBtn);
+            this.filingBox.Location = new System.Drawing.Point(43, 117);
+            this.filingBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.filingBox.Name = "filingBox";
+            this.filingBox.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.filingBox.Size = new System.Drawing.Size(326, 191);
+            this.filingBox.TabIndex = 12;
+            this.filingBox.TabStop = false;
+            this.filingBox.Text = "Filing Status";
+            // 
+            // headOfHouseRdoBtn
+            // 
+            this.headOfHouseRdoBtn.AutoSize = true;
+            this.headOfHouseRdoBtn.Location = new System.Drawing.Point(9, 137);
+            this.headOfHouseRdoBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.headOfHouseRdoBtn.Name = "headOfHouseRdoBtn";
+            this.headOfHouseRdoBtn.Size = new System.Drawing.Size(172, 24);
+            this.headOfHouseRdoBtn.TabIndex = 4;
+            this.headOfHouseRdoBtn.Text = "Head of Household";
+            this.headOfHouseRdoBtn.UseVisualStyleBackColor = true;
+            this.headOfHouseRdoBtn.CheckedChanged += new System.EventHandler(this.headOfHouseRdoBtn_CheckedChanged);
+            // 
+            // jointlyRdoBtn
+            // 
+            this.jointlyRdoBtn.AutoSize = true;
+            this.jointlyRdoBtn.Location = new System.Drawing.Point(9, 102);
+            this.jointlyRdoBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.jointlyRdoBtn.Name = "jointlyRdoBtn";
+            this.jointlyRdoBtn.Size = new System.Drawing.Size(176, 24);
+            this.jointlyRdoBtn.TabIndex = 2;
+            this.jointlyRdoBtn.Text = "Married Filing Jointly";
+            this.jointlyRdoBtn.UseVisualStyleBackColor = true;
+            this.jointlyRdoBtn.CheckedChanged += new System.EventHandler(this.jointlyRdoBtn_CheckedChanged);
+            // 
+            // separatelyRdoBtn
+            // 
+            this.separatelyRdoBtn.AutoSize = true;
+            this.separatelyRdoBtn.Location = new System.Drawing.Point(10, 66);
+            this.separatelyRdoBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.separatelyRdoBtn.Name = "separatelyRdoBtn";
+            this.separatelyRdoBtn.Size = new System.Drawing.Size(208, 24);
+            this.separatelyRdoBtn.TabIndex = 1;
+            this.separatelyRdoBtn.Text = "Married Filing Separately";
+            this.separatelyRdoBtn.UseVisualStyleBackColor = true;
+            this.separatelyRdoBtn.CheckedChanged += new System.EventHandler(this.separatelyRdoBtn_CheckedChanged);
+            // 
+            // singleRdoBtn
+            // 
+            this.singleRdoBtn.AutoSize = true;
+            this.singleRdoBtn.Location = new System.Drawing.Point(10, 31);
+            this.singleRdoBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.singleRdoBtn.Name = "singleRdoBtn";
+            this.singleRdoBtn.Size = new System.Drawing.Size(78, 24);
+            this.singleRdoBtn.TabIndex = 0;
+            this.singleRdoBtn.Text = "Single";
+            this.singleRdoBtn.UseVisualStyleBackColor = true;
+            this.singleRdoBtn.CheckedChanged += new System.EventHandler(this.singleRdoBtn_CheckedChanged);
+            // 
+            // incomeTxt
+            // 
+            this.incomeTxt.Location = new System.Drawing.Point(218, 68);
+            this.incomeTxt.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.incomeTxt.Name = "incomeTxt";
+            this.incomeTxt.Size = new System.Drawing.Size(148, 26);
+            this.incomeTxt.TabIndex = 11;
+            // 
+            // incomeLbl
+            // 
+            this.incomeLbl.AutoSize = true;
+            this.incomeLbl.Location = new System.Drawing.Point(38, 72);
+            this.incomeLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.incomeLbl.Name = "incomeLbl";
+            this.incomeLbl.Size = new System.Drawing.Size(168, 20);
+            this.incomeLbl.TabIndex = 10;
+            this.incomeLbl.Text = "Enter Taxable Income:";
+            // 
+            // titleLbl
+            // 
+            this.titleLbl.AutoSize = true;
+            this.titleLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLbl.Location = new System.Drawing.Point(64, 31);
+            this.titleLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.titleLbl.Name = "titleLbl";
+            this.titleLbl.Size = new System.Drawing.Size(285, 20);
+            this.titleLbl.TabIndex = 9;
+            this.titleLbl.Text = "2019 Marginal Tax Rate Caclulator";
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.clearBtn.Location = new System.Drawing.Point(218, 334);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(125, 35);
+            this.clearBtn.TabIndex = 18;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // Prog3Form
+            // 
+            this.AcceptButton = this.calcTaxBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.clearBtn;
+            this.ClientSize = new System.Drawing.Size(420, 494);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.taxOutLbl);
+            this.Controls.Add(this.taxLbl);
+            this.Controls.Add(this.marginalRateOutLbl);
+            this.Controls.Add(this.marginalRateLbl);
+            this.Controls.Add(this.calcTaxBtn);
+            this.Controls.Add(this.filingBox);
+            this.Controls.Add(this.incomeTxt);
+            this.Controls.Add(this.incomeLbl);
+            this.Controls.Add(this.titleLbl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Prog3Form";
+            this.Text = "Program 3";
+            this.Load += new System.EventHandler(this.Prog3Form_Load);
+            this.filingBox.ResumeLayout(false);
+            this.filingBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label taxOutLbl;
+        private System.Windows.Forms.Label taxLbl;
+        private System.Windows.Forms.Label marginalRateLbl;
+        private System.Windows.Forms.Button calcTaxBtn;
+        private System.Windows.Forms.GroupBox filingBox;
+        private System.Windows.Forms.RadioButton headOfHouseRdoBtn;
+        private System.Windows.Forms.RadioButton jointlyRdoBtn;
+        private System.Windows.Forms.RadioButton separatelyRdoBtn;
+        private System.Windows.Forms.RadioButton singleRdoBtn;
+        private System.Windows.Forms.TextBox incomeTxt;
+        private System.Windows.Forms.Label incomeLbl;
+        private System.Windows.Forms.Label titleLbl;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Label marginalRateOutLbl;
+    }
+}
+
---
+++ Software Development/CIS 199/Program 3/Program 3 Form.cs
@@ -0,0 +1,226 @@
+﻿//Grading ID: N2466
+//Section Number: CIS 199-75
+//Due Date: 3-28-2019
+//Program Number: 3
+//Description: Calculates tax due and assigns a tax rate.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program3
+{
+    public partial class Prog3Form : Form
+    {
+        public Prog3Form()
+        {
+            InitializeComponent();
+        }
+
+        // Single Filers
+        private const int SINGLE1 = 9700;   // 1st single threshold (LOWEST)
+        private const int SINGLE2 = 39475;  // 2nd single threshold
+        private const int SINGLE3 = 84200;  // 3rd single threshold
+        private const int SINGLE4 = 160725; // 4th single threshold
+        private const int SINGLE5 = 204100; // 5th single threshold
+        private const int SINGLE6 = 510300; // 6th single threshold (HIGHEST)
+
+        //Married Filing Separately
+        private const int SEPARATELY1 = 9700;   // 1st married-separately threshold (LOWEST)
+        private const int SEPARATELY2 = 39475;  // 2nd married-separately threshold
+        private const int SEPARATELY3 = 84200;  // 3rd married-separately threshold
+        private const int SEPARATELY4 = 160725; // 4th married-separately threshold
+        private const int SEPARATELY5 = 204100; // 5th married-separately threshold
+        private const int SEPARATELY6 = 306175; // 6th married-separately threshold (HIGHEST)
+
+        // Married Filing Jointly
+        private const int JOINTLY1 = 19400;  // 1st married-jointly threshold (LOWEST)
+        private const int JOINTLY2 = 78950;  // 2nd married-jointly threshold
+        private const int JOINTLY3 = 168400; // 3rd married-jointly threshold
+        private const int JOINTLY4 = 321450; // 4th married-jointly threshold
+        private const int JOINTLY5 = 408200; // 5th married-jointly threshold
+        private const int JOINTLY6 = 612350; // 6th married-jointly threshold (HIGHEST)
+
+        // Head of Household
+        private const int HOH1 = 13850;  // 1st head of household threshold (LOWEST)
+        private const int HOH2 = 52850;  // 2nd head of household threshold
+        private const int HOH3 = 84200;  // 3rd head of household threshold
+        private const int HOH4 = 160700; // 4th head of household threshold
+        private const int HOH5 = 204100; // 5th head of household threshold
+        private const int HOH6 = 510300; // 6th head of household threshold (HIGHEST)
+
+        // Income threshold values that apply to this filer
+        private int threshold1; // 1st income threshold
+        private int threshold2; // 2nd income threshold
+        private int threshold3; // 3rd income threshold
+        private int threshold4; // 4th income threshold
+        private int threshold5; // 5th income threshold
+        private int threshold6; // 6th income threshold
+
+        //Calculate button event handler
+        private void calcTaxBtn_Click(object sender, EventArgs e)
+        {
+            // Tax rates
+            const decimal RATE1 = 0.10m; // 1st tax rate (LOWEST)
+            const decimal RATE2 = 0.12m; // 2nd tax rate
+            const decimal RATE3 = 0.22m; // 3rd tax rate
+            const decimal RATE4 = 0.24m; // 4th tax rate
+            const decimal RATE5 = 0.32m; // 5th tax rate
+            const decimal RATE6 = 0.35m; // 6th tax rate
+            const decimal RATE7 = 0.37m; // 7th tax rate (HIGHEST)
+
+            // Naming arrays
+            decimal[] rates = { RATE1, RATE2, RATE3, RATE4, RATE5, RATE6, RATE7 }; // Tax rates array
+            int[] thresholds = new int[rates.Length]; // Creating new array with rate length
+
+            // Assigning values to thresholds array
+            thresholds[0] = 1;              // Assigning threshold position 0
+            thresholds[1] = threshold1 + 1; // Assigning threshold position 1
+            thresholds[2] = threshold2 + 1; // Assigning threshold position 2
+            thresholds[3] = threshold3 + 1; // Assigning threshold position 3
+            thresholds[4] = threshold4 + 1; // Assigning threshold position 4
+            thresholds[5] = threshold5 + 1; // Assigning threshold position 5
+            thresholds[6] = threshold6 + 1; // Assigning threshold position 6
+
+            // Naming variables
+            bool found = false; // Found bool variable
+            int validIncomeInput; // User's input
+            decimal assignedTax = 0m; // Assume lowest rate for bad input
+
+            // Starting at the correct position
+            int index = thresholds.Length - 1;
+
+            // Declaring variables
+            decimal tax = 0; // Filer's calculated income tax due
+            decimal currentTax = 0; // Filer's tax for current tier
+
+            //If statement for valid input
+            if (int.TryParse(incomeTxt.Text, out validIncomeInput) && validIncomeInput >= 0)
+            {
+                // While loop to search through the arrays
+                while (index >= 0 && !found)
+                {
+                    if (validIncomeInput >= thresholds[index])
+                    {
+                        found = true;
+                        assignedTax = rates[index];
+                    }
+                    else
+                    {
+                        --index;
+                    }
+                }
+
+                if (index == thresholds.Length - 1)
+                {
+                    currentTax = (validIncomeInput - threshold6) * RATE7;
+                    --index;
+                    tax += currentTax;
+                }
+
+                // Calculation loop
+                if (int.TryParse(incomeTxt.Text, out validIncomeInput)) // Won't start without proper value
+                {
+                    for (int counter = 0; counter <= index; ++counter)
+                    {
+                        currentTax = Math.Min((validIncomeInput - (thresholds[counter] - 1)), (thresholds[counter + 1] - thresholds[counter])) * rates[counter];
+                        tax += currentTax;
+                    }
+                }
+
+                // Output statements
+                if (found && int.TryParse(incomeTxt.Text, out validIncomeInput)) //Won't output defaut for incorrect input
+                {
+                    marginalRateOutLbl.Text = ($"{assignedTax:P0}");
+                    taxOutLbl.Text = ($"{tax:C}");
+                }
+            }
+
+            // Error message for invalid input
+            else
+            {
+                    MessageBox.Show("Enter a valid income.");
+            }
+               
+        }
+
+        // Sets default filing status as Single
+        private void Prog3Form_Load(object sender, EventArgs e)
+        {
+            {
+             singleRdoBtn.Checked = true; // Single by default, will raise CheckedChanged event
+            }
+        }
+
+        // User has checked/unchecked Single radio button
+        // Updates income thresholds
+        private void singleRdoBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            if (singleRdoBtn.Checked)
+            {
+                threshold1 = SINGLE1;
+                threshold2 = SINGLE2;
+                threshold3 = SINGLE3;
+                threshold4 = SINGLE4;
+                threshold5 = SINGLE5;
+                threshold6 = SINGLE6;
+            }
+        }
+
+        // User has checked/unchecked Married Filing Separately radio button
+        // Updates income thresholds
+        private void separatelyRdoBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            {
+                threshold1 = SEPARATELY1;
+                threshold2 = SEPARATELY2;
+                threshold3 = SEPARATELY3;
+                threshold4 = SEPARATELY4;
+                threshold5 = SEPARATELY5;
+                threshold6 = SEPARATELY6;
+            }
+        }
+
+        // User has checked/unchecked Married Filing Jointly radio button
+        // Updates income thresholds
+        private void jointlyRdoBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            {
+                threshold1 = JOINTLY1;
+                threshold2 = JOINTLY2;
+                threshold3 = JOINTLY3;
+                threshold4 = JOINTLY4;
+                threshold5 = JOINTLY5;
+                threshold6 = JOINTLY6;
+            }
+        }
+
+        // User has checked/unchecked Head of Household radio button
+        // Updates income thresholds
+        private void headOfHouseRdoBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            {
+                threshold1 = HOH1;
+                threshold2 = HOH2;
+                threshold3 = HOH3;
+                threshold4 = HOH4;
+                threshold5 = HOH5;
+                threshold6 = HOH6;
+            }
+        }
+
+        //Clear form button
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            incomeTxt.Text = "";
+            marginalRateOutLbl.Text = "";
+            taxOutLbl.Text = "";
+        }
+    }
+}
