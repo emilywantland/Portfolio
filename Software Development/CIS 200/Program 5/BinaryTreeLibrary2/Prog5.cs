d9fafae852c055a9f452a611cc4effb2fe8b5757
Emily Wantland <50221752+emilywantland@users.noreply.github.com>
2020-11-06 12:31:01 -0500

    Add files via upload

---
+++ Software Development/CIS 200/Program 5/BinaryTreeLibrary2/Prog5.cs
@@ -0,0 +1,105 @@
+﻿// Program 5
+// CIS 200-01
+// Fall 2019
+// Due: 12/3/2019
+// By: M1791
+
+// File: Prog5.cs
+// Declaration of class TreeNode and class Tree.
+
+using System;
+
+namespace Prog5
+{
+   // class TreeNode Declaration
+   class TreeNode<T> where T : IComparable
+   {
+      // Automatic Property LeftNode
+      public TreeNode<T> LeftNode { get; set; }
+
+      // Automatic Property Data
+      public IComparable Data { get; private set; }
+
+      // Automatic Property RightNode
+      public TreeNode<T> RightNode { get; set; }
+
+      // Initialize Data and make this a Leaf Node
+      public TreeNode(IComparable nodeData)
+      {
+         Data = nodeData;
+      }
+
+      // Insert TreeNode into Tree that contains Nodes;
+      // Ignore Duplicate Values
+      public void Insert(IComparable insertValue)
+      {
+         if (insertValue.CompareTo(Data) < 0) // Insert in Left Subtree
+         {
+            // Insert New TreeNode
+            if (LeftNode == null)
+            {
+               LeftNode = new TreeNode<T>(insertValue);
+            }
+            else // Continue Traversing Left Subtree
+            {
+               LeftNode.Insert(insertValue);
+            }
+         }
+         else if (insertValue.CompareTo(Data) > 0) // Insert in Right
+         {
+            // Insert New TreeNode
+            if (RightNode == null)
+            {
+               RightNode = new TreeNode<T>(insertValue);
+            }
+            else // Continue Traversing Right Subtree
+            {
+               RightNode.Insert(insertValue);
+            }
+         }
+      }
+   }
+
+   // Class Tree Declaration
+   public class Tree<T> where T : IComparable
+   {
+      private TreeNode<T> root;
+
+      // Insert a new node in the binary search tree.
+      // If the root node is null, create the root node here.
+      // Otherwise, call the insert method of class TreeNode.
+      public void InsertNode(IComparable insertValue)
+      {
+         if (root == null)
+         {
+            root = new TreeNode<T>(insertValue);
+         }
+         else
+         {
+            root.Insert(insertValue);
+         }
+      }
+
+      // Begin Inorder Traversal
+      public void InorderTraversal()
+      {
+         InorderHelper(root);
+      }
+
+      // Recursive Method to Perform Inorder Traversal
+      private void InorderHelper(TreeNode<T> node)
+      {
+         if (node != null)
+         {
+            // Traverse Left Subtree
+            InorderHelper(node.LeftNode);
+
+            // Output Node Data
+            Console.Write($"{node.Data} ");
+
+            // Traverse Right Subtree
+            InorderHelper(node.RightNode);
+         }
+      }
+   }
+}
---
+++ Software Development/CIS 200/Program 5/BinaryTreeLibrary2/TestProg.cs
@@ -0,0 +1,73 @@
+﻿// Program 5
+// CIS 200-01
+// Fall 2019
+// Due: 12/3/2019
+// By: M1791
+
+// File: TestProg.cs
+// Testing class Tree with IComparable objects.
+
+using System;
+using Prog5;
+
+// Class TreeTest Declaration
+class TestProg
+{
+    // Test Class Tree
+    static void Main()
+    {
+        int[] intArray = { 8, 2, 4, 3, 1, 7, 5, 6, 10, 9 }; // Array of ints
+        double[] doubleArray = { 8.8, 2.2, 4.4, 3.3, 1.1, 7.7, 5.5, 6.6, 10.10, 9.9}; // Array of doubles
+        string[] stringArray = {"eight", "two", "four", "three", "one", "seven", "five", "six", "ten", "nine"}; // Array of strings
+
+        // Create int Tree
+        Tree<double> intTree = new Tree<double>();
+        PopulateTree(intArray, intTree, nameof(intTree));
+        TraverseTree(intTree, nameof(intTree));
+
+        // Create double Tree
+        Tree<double> doubleTree = new Tree<double>();
+        PopulateTree(doubleArray, doubleTree, nameof(doubleTree));
+        TraverseTree(doubleTree, nameof(doubleTree));
+
+        // Create string Tree
+        Tree<double> stringTree = new Tree<double>();
+        PopulateTree(stringArray, stringTree, nameof(stringTree));
+        TraverseTree(stringTree, nameof(stringTree));
+    }
+
+    // Populate Tree with Array Elements
+    private static void PopulateTree<T>(T[] array, Tree<double> tree, string name) where T : IComparable<T>
+    {
+        Console.WriteLine($"Inserting into {name}:");
+
+        foreach (IComparable data in array)
+        {
+            Console.Write($"{data} ");
+            tree.InsertNode(data);
+        }
+        Console.WriteLine();
+        Pause();
+    }
+
+    // Perform Traversals
+    private static void TraverseTree(Tree<double> tree, string treeType)
+    {
+        // Perform Inorder Traversal of Tree
+        Console.WriteLine($"Inorder traversal of {treeType}");
+        tree.InorderTraversal();
+        Console.WriteLine();
+        Pause();
+    }
+
+    // Precondition:  None
+    // Postcondition: Pauses program execution until user presses Enter and
+    //                then clears the screen
+    public static void Pause()
+    {
+        Console.WriteLine("Press Enter to Continue...");
+        Console.ReadLine();
+
+        Console.Clear();
+    }
+}
